445a59041656d0a9037154cdc38625cf4133dbc4
mcsmith01 <mcsmith01@alumni.amherst.edu>
2018-09-12 10:05:38 -0400

    k9/12 - Marcus

--- BankAccount.java
+++ BankAccount.java
@@ -26,4 +26,22 @@ public class BankAccount {
 		return balance;
 	}
 
+	public double withdraw(double amount, String pass) {
+		if (amount >= 0 && pass.equals(password) && amount <= balance) {
+			balance -= amount;
+			return balance;
+		} else {
+			return -1;
+		}
+	}
+
+	public double accrueInterest(int month) {
+		double monthRate = interestRate/12 * month; /*interest*/
+		double interest = balance * monthRate; /*balance with interest*/
+		balance = balance + interest; /*balance plus interest made*/
+		return balance;
+	}
+
+
+
 }
--- BankRunner.java
+++ BankRunner.java
@@ -7,7 +7,11 @@ public class BankRunner {
 		ba.deposit(100);
 		System.out.println(ba.getBalance("password"));
 		ba.deposit(-500);
-		System.out.println(ba.getBalance("password"));		
+		System.out.println(ba.getBalance("password"));
+		ba.withdraw(20, "password");
+		System.out.println(ba.getBalance("password"));
+		System.out.println(ba.accrueInterst(2));
+
 	}
 
 }
