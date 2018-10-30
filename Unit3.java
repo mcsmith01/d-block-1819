9d151645d54f0cf88407b8b47c5bcf6fd3c10dda
Chase Smith <smithcha@Chases-Air.berkeleyprep.org; accounts.berkeleyprep.org>
2018-10-30 10:42:56 -0400

    10/30 - George - For loops

--- Unit3.java
+++ Unit3.java
@@ -1,23 +1,42 @@
 public class Unit3 {
 
   public static void main(String[] args) {
-    int x = sumOfMinutesChenzoIsLate(90);
-    System.out.println(x);
-    timesDevProvesHesGreat("everyday");
-    int y = exponents(3, 5);
-    System.out.println(y);
+    devIsSmall(22); devIsSwole(22);
+    // int x = sumOfMinutesChenzoIsLate(90);
+    // System.out.println(x);
+    // timesDevProvesHesGreat("everyday");
+    // int y = exponents(3, 5);
+    // System.out.println(y);
   }
   public static int sumOfMinutesChenzoIsLate(int num) {
     int initialSum = 0;
-    int minutesLate = 1;
-    while(minutesLate <= num) {
+    //int minutesLate = 1;
+    // while(minutesLate <= num) {
+    //   initialSum += minutesLate;
+    //   minutesLate++;
+    //
+    // }
+    for (int minutesLate = 1; minutesLate <= num; minutesLate++){
       initialSum += minutesLate;
-      minutesLate++;
-
     }
     return initialSum;
 
   }
+  public static void devIsSmall(int num1){ for (int i = 0; i <= num1; i++){ System.out.println(i);} }
+
+  public static void devIsSwole(int evens){
+    for (int i = 0; i <= evens; i++){
+      if (i % 2 == 0){
+        System.out.print(i+" ");
+      }
+    }
+    System.out.println();
+    for (int i = 0; i <= evens; i+=2){
+      System.out.print(i + " ");
+    }
+  }
+  
+
   public static void timesDevProvesHesGreat(String allTheTime) {
     int end = allTheTime.length();
     while(0 != end){
@@ -33,9 +52,9 @@ public class Unit3 {
       currentResult = currentResult * base;
       iteration++;
     }
-    while(iteration > 0) {
-      System.out.println("class is over");
-    }
+    // while(iteration > 0) {
+    //   System.out.println("class is over");
+    // }
     return currentResult;
 
   }
