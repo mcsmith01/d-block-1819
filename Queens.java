abcb984215cc082202d0891acc0cddb521d16bd6
M. Chase Smith <smithcha@berkeleyprep.org>
2019-02-26 08:04:19 -0500

    2/25 - Justin - Finished 8 Queens

--- Queens.java
+++ Queens.java
@@ -1,4 +1,15 @@
 public class Queens {
+
+  private static int count;
+
+  public static void main(String[] args){
+    int[][] board = new int[8][8];
+    count = 0;
+    queenPlacement(board, 0);
+    System.out.println(count);
+    System.out.println(skills(25, 5));
+  }
+
   public static boolean isLegal(int[][] board, int r, int c){
     for(int i = 0; i < r; i++){
       if(board[i][c] == 1){
@@ -16,7 +27,9 @@ public class Queens {
   }
   public static boolean queenPlacement(int[][] board, int r){
     if(r==board.length){
-      return true;
+      printBoard(board);
+      count++;
+      return false;
     }
     for(int i = 0; i < board[r].length; i++){
       if(isLegal(board, r, i)){
@@ -29,5 +42,39 @@ public class Queens {
       }
     }
     return false;
+  }
+
+  public static void printBoard(int[][] board){
+    for(int i = 0; i < board.length; i++){
+      for(int j = 0; j < board[0].length; j++){
+        if(board[i][j] == 0){
+          System.out.print("_ ");
+        }else{
+          System.out.print("q ");
+        }
+      }
+      System.out.println();
+    }
+    System.out.println();
+  }
+
+  public static int skills(int n, int m) {
+    System.out.println(n + " " + m);
+    if (n % 2 == 0) {
+      return skills(n / 2) + m;
+    } else {
+      return skills(n - 1, m - 1);
+    }
+  }
+
+  public static int skills(int n) {
+    System.out.println(n);
+    if (n == 0) {
+      return 0;
+    } else if (n % 2 == 0) {
+      return skills(n, n / 2);
+    } else {
+      return skills(n - 1);
+    }
   }
 }
